c3611dd21977571e87071ff571880ac2b94754ce
SirTom <sirvos.lol@gmail.com>
2022-11-24 17:52:38 +0100

    first

---
+++ index.php
@@ -0,0 +1,30 @@
+<? //php session_start();?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8"> <!--Meta-->
+    <meta name="description" content="">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title></title> <!--title of page-->
+</head>
+<body>
+<header>
+    <a href="index.php"><img src="assets/img/house-solid.svg" alt="house" id="house"></a>
+    <h1>Accueil BoutiqueBasique en MVC1 !</h1>
+    <nav>
+        <ul>
+
+            <li>
+                <a href='controllers/Controller.php?todo=afficherProduits'>Voir les produits</a>
+            </li>
+
+        </ul>
+    </nav>
+</header>
+</body>
+</html>
